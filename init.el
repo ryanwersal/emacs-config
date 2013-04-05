e9556b92bbf74f41b7ea9ec822674196c8dbf236
Ryan Wersal <ryan@ryanwersal.com>
2013-04-05 00:37:31 -0500

    Updated markdown-mode file types and added basic snippets

--- init.el
+++ init.el
@@ -280,6 +280,7 @@ If already there, go to actual beginning of line."
 (add-to-list 'auto-mode-alist '("\\.js\\'" . js2-mode))
 
 (add-to-list 'auto-mode-alist '("\\.md\\'" . markdown-mode))
+(add-to-list 'auto-mode-alist '("\\.markdown\\'" . markdown-mode))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Start Emacs Server
