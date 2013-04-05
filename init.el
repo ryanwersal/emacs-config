81800fc8da649d7e439bb5a0c10c942d12c8ab13
Ryan Wersal <ryan@ryanwersal.com>
2013-04-05 17:11:30 -0500

    Removed multiple-cursors

--- init.el
+++ init.el
@@ -217,12 +217,6 @@ If already there, go to actual beginning of line."
 
 (autoload 'markdown-mode "markdown-mode" "Major mode for editing Markdown files" t)
 
-;; Configure multiple-cursors
-(require 'multiple-cursors)
-(global-set-key (kbd "C->") 'mc/mark-next-like-this)
-(global-set-key (kbd "C-<") 'mc/mark-previous-like-this)
-(global-set-key (kbd "C-c C-<") 'mc/mark-all-like-this)
-
 (require 'expand-region)
 (global-set-key (kbd "C-h") 'er/expand-region)
 
