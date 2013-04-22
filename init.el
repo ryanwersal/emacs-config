ec6d16ac35d75cce1bf990e98a3dac6079169760
Ryan Wersal <ryan@ryanwersal.com>
2013-04-22 14:41:18 -0500

    Treat each part of CamelCase as a word.

--- init.el
+++ init.el
@@ -245,6 +245,7 @@ If already there, go to actual beginning of line."
 ;; Configure Modes
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 (add-hook 'prog-mode-hook 'flyspell-prog-mode) ;; Spell check comments.
+(add-hook 'prog-mode-hook 'subword-mode) ;; Make each part of CamelCase a word.
 
 (add-hook 'c-mode-common-hook
 		  (lambda ()
