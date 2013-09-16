ba9c8c1b854b0a7fa93b76390312db49d1ee71f0
Ryan Wersal <ryan@ryanwersal.com>
2013-09-16 09:42:48 -0500

    And even more package updates

--- elpa/dash-20130818.2143/dash-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "dash" "20130818.2143" "A modern list library for Emacs [github]" (quote nil))
---
+++ elpa/helm-20130916.950/helm-pkg.el
@@ -0,0 +1,2 @@
+(define-package "helm" "20130916.950" "Helm is an Emacs incremental and narrowing framework [github]" 'nil)
+
