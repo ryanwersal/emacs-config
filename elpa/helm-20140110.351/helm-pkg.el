e42e56c155827ad380ca044a1fbbc5817cc32429
Ryan Wersal <ryan@ryanwersal.com>
2014-01-10 16:48:10 -0600

    Package updates

---
+++ elpa/helm-20140110.351/helm-pkg.el
@@ -0,0 +1,1 @@
+(define-package "helm" "20140110.351" "Helm is an Emacs incremental and narrowing framework" 'nil)
---
+++ elpa/helm-c-yasnippet-20140108.2238/helm-c-yasnippet-pkg.el
@@ -0,0 +1,1 @@
+(define-package "helm-c-yasnippet" "20140108.2238" "helm source for yasnippet.el" (quote nil))
