e7d786137922e7693adfcc963ce266fc8d9e5aeb
Ryan Wersal <ryan@ryanwersal.com>
2014-01-28 20:32:06 -0600

    Package updates

---
+++ elpa/auto-complete-20140127.632/auto-complete-autoloads.el
@@ -0,0 +1,29 @@
+;;; auto-complete-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+
+;;;### (autoloads (auto-complete) "auto-complete" "auto-complete.el"
+;;;;;;  (21224 26601 866708 862000))
+;;; Generated autoloads from auto-complete.el
+
+(autoload 'auto-complete "auto-complete" "\
+Start auto-completion at current point.
+
+\(fn &optional SOURCES)" t nil)
+
+;;;***
+
+;;;### (autoloads nil nil ("auto-complete-config.el" "auto-complete-pkg.el")
+;;;;;;  (21224 26601 879582 142000))
+
+;;;***
+
+(provide 'auto-complete-autoloads)
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; auto-complete-autoloads.el ends here
