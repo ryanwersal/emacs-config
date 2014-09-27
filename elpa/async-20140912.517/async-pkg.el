2e3860affd19b3cefa73e95b876898cae39e523e
Ryan Wersal <ryan@ryanwersal.com>
2014-09-27 15:07:28 -0500

    Package updates (including fix for helm styling)

--- elpa/async-20140912.517/async-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "async" "20140912.517" "Asynchronous processing in Emacs" 'nil)
--- elpa/clojure-mode-20140914.556/clojure-mode-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "clojure-mode" "20140914.556" "Major mode for Clojure code" (quote ((emacs "24.1"))))
