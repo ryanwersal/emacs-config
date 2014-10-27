8e10dae9e24415c85d9fee44d762298e7ea87e8e
Ryan Wersal <ryan@ryanwersal.com>
2014-10-27 15:53:26 -0500

    Update melpa URL

--- init.el
+++ init.el
@@ -181,7 +181,7 @@ If already there, go to actual beginning of line."
 (require 'package)
 (setq package-archives '(("gnu" . "http://elpa.gnu.org/packages/")
 						 ("marmalade" . "http://marmalade-repo.org/packages/")
-						 ("melpa" . "http://melpa.milkbox.net/packages/")))
+						 ("melpa" . "http://melpa.org/packages/")))
 (package-initialize)
 
 (require 'async-bytecomp)
